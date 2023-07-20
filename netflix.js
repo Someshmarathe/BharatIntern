d177d3f430a6f3583e433ed733d472ea2d2841ce
Somesh Marathe <138389930+Someshmarathe@users.noreply.github.com>
2023-07-20 15:13:56 +0530

    Add files via upload

---
+++ netflix.js
@@ -0,0 +1,54 @@
+function call(){
+    document.getElementById("hindi").style.opacity ="1";
+}
+function callno(){
+    document.getElementById("hindi").style.opacity ="0";
+}
+function callchange(){
+    document.getElementById("change").innerHTML="hindi";
+    document.getElementById("change").style.width="150px";
+    document.getElementById("hindi").innerHTML="English";
+}
+function callbl(){
+    document.getElementById("change").innerHTML="English";
+    document.getElementById("hindi").innerHTML="Hindi";
+}
+function calldetailssh1(){
+    document.getElementById("show1").innerHTML = "Netflix is a subscription-based streaming service that allows our members to watch TV shows and movies on an internet-connected device.";
+}
+function calldetailsclosesh1(){
+    document.getElementById("show1").innerHTML = " ";
+}
+function calldetails2(){
+    document.getElementById("show2").innerHTML = "The Netflix Standard Subscription Plan costs Rs 499 per month and allows users to stream on up to two devices, including phone, tablet, computer and smart TV, at a time in Full High definition (Full HD) 1080p quality.";
+}
+function calldetailsclose2(){
+    document.getElementById("show2").innerHTML = " ";
+}
+function calldetails3(){
+    document.getElementById("show3").innerHTML = "Sign in with your Netflix account to watch instantly on the web at netflix.com from your personal computer or on any internet-connected device that offers the Netflix app, including smart TVs, smartphones, tablets, streaming media players and game consoles.";
+}
+function calldetailsclose3(){
+    document.getElementById("show3").innerHTML = " ";
+}
+function calldetails4(){
+    document.getElementById("show4").innerHTML = "Visit Netflix.com on any web browser and sign in to your account.";
+}
+function calldetailsclose4(){
+    document.getElementById("show4").innerHTML = " ";
+}
+function calldetails5(){
+    document.getElementById("show5").innerHTML = "hello india";
+    document.getElementById("show5").style.marginTop ="-20px";
+}
+function calldetailsclose5(){
+    document.getElementById("show5").innerHTML = " ";
+}
+function calldetails6(){
+    document.getElementById("show6").innerHTML = "hello india";
+    document.getElementById("show6").style.marginTop ="-20px";
+}
+function calldetailsclose6(){
+    document.getElementById("show6").innerHTML =" ";
+
+}
